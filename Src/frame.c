c758c9a5e6131f5dbc752ea7a92921a21abed4ee
speck <speck2@llnl.gov>
1998-08-20 17:40:28 +0000

    Added code to support arbitrary global tensor transformation including modification of transform_tensor() to operate in a loop, addition of transform_stress_strain(), and addition of transpose_tensors().

--- Src/frame.c
+++ Src/frame.c
@@ -12,6 +12,9 @@
 #include "viewer.h"
 
 
+static Bool_type transpose_tensors();
+
+
 /************************************************************
  * TAG( global_to_local_mtx )
  *
@@ -67,10 +70,10 @@ float localMat[3][3];
 
        
 /************************************************************
- * TAG( transform_tensor )
+ * TAG( transform_tensors )
  *
- * Transform the stress/strain tensor with a change-of-coordinates
- * transformation matrix.
+ * Transform an array of stress/strain tensors with a 
+ * change-of-coordinates transformation matrix.
  *
  *                  t
  *     sigma_new = A * sigma * A
@@ -79,35 +82,137 @@ float localMat[3][3];
  * the above matrix multiply.
  */
 void
-transform_tensor( ten, mat )
-float ten[6];
-float mat[3][3];
+transform_tensors( qty, tensors, mat )
+int qty;
+float *tensors;
+float mat[][3];
 {
     float new[6], sub1[3], sub2[3], sub3[3];
-    int i;
+    float *p_ten;
+    int i, j;
 
-    /* Compute common sub-expressions. */
-    sub1[0] = ten[0]*mat[0][0] + ten[3]*mat[1][0] + ten[5]*mat[2][0];
-    sub1[1] = ten[3]*mat[0][0] + ten[1]*mat[1][0] + ten[4]*mat[2][0];
-    sub1[2] = ten[5]*mat[0][0] + ten[4]*mat[1][0] + ten[2]*mat[2][0];
+    p_ten = tensors;
+    for ( i = 0; i < qty; i++ )
+    {
+        /* Compute common sub-expressions. */
+        sub1[0] = p_ten[0]*mat[0][0] + p_ten[3]*mat[1][0] + p_ten[5]*mat[2][0];
+        sub1[1] = p_ten[3]*mat[0][0] + p_ten[1]*mat[1][0] + p_ten[4]*mat[2][0];
+        sub1[2] = p_ten[5]*mat[0][0] + p_ten[4]*mat[1][0] + p_ten[2]*mat[2][0];
 
-    sub2[0] = ten[0]*mat[0][1] + ten[3]*mat[1][1] + ten[5]*mat[2][1];
-    sub2[1] = ten[3]*mat[0][1] + ten[1]*mat[1][1] + ten[4]*mat[2][1];
-    sub2[2] = ten[5]*mat[0][1] + ten[4]*mat[1][1] + ten[2]*mat[2][1];
+        sub2[0] = p_ten[0]*mat[0][1] + p_ten[3]*mat[1][1] + p_ten[5]*mat[2][1];
+        sub2[1] = p_ten[3]*mat[0][1] + p_ten[1]*mat[1][1] + p_ten[4]*mat[2][1];
+        sub2[2] = p_ten[5]*mat[0][1] + p_ten[4]*mat[1][1] + p_ten[2]*mat[2][1];
 
-    sub3[0] = ten[0]*mat[0][2] + ten[3]*mat[1][2] + ten[5]*mat[2][2];
-    sub3[1] = ten[3]*mat[0][2] + ten[1]*mat[1][2] + ten[4]*mat[2][2];
-    sub3[2] = ten[5]*mat[0][2] + ten[4]*mat[1][2] + ten[2]*mat[2][2];
+        sub3[0] = p_ten[0]*mat[0][2] + p_ten[3]*mat[1][2] + p_ten[5]*mat[2][2];
+        sub3[1] = p_ten[3]*mat[0][2] + p_ten[1]*mat[1][2] + p_ten[4]*mat[2][2];
+        sub3[2] = p_ten[5]*mat[0][2] + p_ten[4]*mat[1][2] + p_ten[2]*mat[2][2];
 
-    /* Get the transformed vector. */
-    new[0] = mat[0][0]*sub1[0] + mat[1][0]*sub1[1] + mat[2][0]*sub1[2];
-    new[1] = mat[0][1]*sub2[0] + mat[1][1]*sub2[1] + mat[2][1]*sub2[2];
-    new[2] = mat[0][2]*sub3[0] + mat[1][2]*sub3[1] + mat[2][2]*sub3[2];
-    new[3] = mat[0][0]*sub2[0] + mat[1][0]*sub2[1] + mat[2][0]*sub2[2];
-    new[4] = mat[0][1]*sub3[0] + mat[1][1]*sub3[1] + mat[2][1]*sub3[2];
-    new[5] = mat[0][0]*sub3[0] + mat[1][0]*sub3[1] + mat[2][0]*sub3[2];
+        /* Get the transformed vector. */
+        new[0] = mat[0][0]*sub1[0] + mat[1][0]*sub1[1] + mat[2][0]*sub1[2];
+        new[1] = mat[0][1]*sub2[0] + mat[1][1]*sub2[1] + mat[2][1]*sub2[2];
+        new[2] = mat[0][2]*sub3[0] + mat[1][2]*sub3[1] + mat[2][2]*sub3[2];
+        new[3] = mat[0][0]*sub2[0] + mat[1][0]*sub2[1] + mat[2][0]*sub2[2];
+        new[4] = mat[0][1]*sub3[0] + mat[1][1]*sub3[1] + mat[2][1]*sub3[2];
+        new[5] = mat[0][0]*sub3[0] + mat[1][0]*sub3[1] + mat[2][0]*sub3[2];
 
-    for ( i = 0; i < 6; i++ )
-        ten[i] = new[i];
+        for ( j = 0; j < 6; j++ )
+            p_ten[j] = new[j];
+        
+        /* Increment to point to the next element's tensor. */
+        p_ten += 6;
+    }
 }
+
+
+/************************************************************
+ * TAG( transform_stress_strain )
+ *
+ * Transform stress or strain tensors for hex or shell 
+ * elements at the current state according to the currently 
+ * defined coordinate transformation matrix.
+ *
+ * The transformed components are left in analy->tmp_result.
+ */
+Bool_type
+transform_stress_strain( which, analy )
+int which;
+Analysis *analy;
+{
+    int qty;
+    Bool_type rval;
+
+    /* Load the tensors for all elements from the database. */
+    rval = get_tensor_result( analy->cur_state, which, 
+                              analy->tmp_result[0] );
+    if ( !rval )
+        return FALSE;
+    
+    qty = ( which == VAL_HEX_SIGX ) ? analy->geom_p->bricks->cnt
+                                    : analy->geom_p->shells->cnt;
+    
+    
+    /* Do it... */
+    transform_tensors( qty, analy->tmp_result[0],
+                       analy->tensor_transform_matrix );
+    
+    /*
+     * Re-order tensors into separate component arrays (i.e., x,
+     * y, z, xy, yz, zx) before returning.
+     */
+    return transpose_tensors( qty, analy );
+}
+
+
+/************************************************************
+ * TAG( transpose_tensors )
+ *
+ * Transpose the tensors in analy->tmp_result[0], storing back
+ * into analy->tmp_result[0...5].
+ */
+static Bool_type
+transpose_tensors( qty, analy )
+int qty;
+Analysis *analy;
+{
+    float *p_tmp, *p_t0, *p_t1, *p_t2, *p_t3, *p_t4, *p_t5;
+    float (*tens)[6];
+    double *p_dsrc, *p_dbound, *p_ddest;
+    int i;
+    
+    p_tmp = NEW_N( float, qty * 6, "Temp tensors array" );
+    if ( p_tmp == NULL )
+        return FALSE;
+    
+    /* Copy array of tensors to temp storage with double-sized moves. */
+    p_dsrc = (double *) analy->tmp_result[0];
+    p_dbound = (double *) (analy->tmp_result[0] + qty * 6);
+    p_ddest = (double *) p_tmp;
+    for ( ; p_dsrc < p_dbound; *p_ddest++ = *p_dsrc++ );
+    
+    /* Now transpose back into analy->tmp_result. */
+    
+    /* Assign local pointers. */
+    p_t0 = analy->tmp_result[0];
+    p_t1 = analy->tmp_result[1];
+    p_t2 = analy->tmp_result[2];
+    p_t3 = analy->tmp_result[3];
+    p_t4 = analy->tmp_result[4];
+    p_t5 = analy->tmp_result[5];
+    
+    tens = (float (*)[6]) p_tmp;
+    for ( i = 0; i < qty; i++ )
+    {
+        p_t0[i] = tens[i][0];
+        p_t1[i] = tens[i][1];
+        p_t2[i] = tens[i][2];
+        p_t3[i] = tens[i][3];
+        p_t4[i] = tens[i][4];
+        p_t5[i] = tens[i][5];
+    }
+    
+    free( p_tmp );
+    
+    return TRUE;
+}
+
 
