7df30d692922e1262fe33971ba1a857f6b9d78a6
Ryan Hathaway <hathaway6@llnl.gov>
2024-10-28 13:28:05 -0700

    Resolve "Additional changes for M_INODE superclass"

--- src/mili_fparam.h
+++ src/mili_fparam.h
@@ -1,62 +1,62 @@
 !.....MILI parameters include file
 
-! Copyright (c) 2016, Lawrence Livermore National Security, LLC. 
-! Produced at the Lawrence Livermore National Laboratory. Written 
-! by Kevin Durrenberger: durrenberger1@llnl.gov. CODE-OCEC-16-056. 
+! Copyright (c) 2016, Lawrence Livermore National Security, LLC.
+! Produced at the Lawrence Livermore National Laboratory. Written
+! by Kevin Durrenberger: durrenberger1@llnl.gov. CODE-OCEC-16-056.
 ! All rights reserved.
 
-! This file is part of Mili. For details, see <URL describing code 
+! This file is part of Mili. For details, see <URL describing code
 ! and how to download source>.
 
-! Please also read this link-- Our Notice and GNU Lesser General 
+! Please also read this link-- Our Notice and GNU Lesser General
 ! Public License.
 
-! This program is free software; you can redistribute it and/or modify 
-! it under the terms of the GNU General Public License (as published by 
+! This program is free software; you can redistribute it and/or modify
+! it under the terms of the GNU General Public License (as published by
 ! the Free Software Foundation) version 2.1 dated February 1999.
 
-! This program is distributed in the hope that it will be useful, but 
-! WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF 
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms 
+! This program is distributed in the hope that it will be useful, but
+! WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms
 ! and conditions of the GNU General Public License for more details.
- 
-! You should have received a copy of the GNU Lesser General Public License 
-! along with this program; if not, write to the Free Software Foundation, 
+
+! You should have received a copy of the GNU Lesser General Public License
+! along with this program; if not, write to the Free Software Foundation,
 ! Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- 
-! This work was produced at the University of California, Lawrence 
-! Livermore National Laboratory (UC LLNL) under contract no. 
-! W-7405-ENG-48 (Contract 48) between the U.S. Department of Energy 
-! (DOE) and The Regents of the University of California (University) 
-! for the operation of UC LLNL. Copyright is reserved to the University 
-! for purposes of controlled dissemination, commercialization through 
-! formal licensing, or other disposition under terms of Contract 48; 
-! DOE policies, regulations and orders; and U.S. statutes. The rights 
-! of the Federal Government are reserved under Contract 48 subject to 
-! the restrictions agreed upon by the DOE and University as allowed 
+
+! This work was produced at the University of California, Lawrence
+! Livermore National Laboratory (UC LLNL) under contract no.
+! W-7405-ENG-48 (Contract 48) between the U.S. Department of Energy
+! (DOE) and The Regents of the University of California (University)
+! for the operation of UC LLNL. Copyright is reserved to the University
+! for purposes of controlled dissemination, commercialization through
+! formal licensing, or other disposition under terms of Contract 48;
+! DOE policies, regulations and orders; and U.S. statutes. The rights
+! of the Federal Government are reserved under Contract 48 subject to
+! the restrictions agreed upon by the DOE and University as allowed
 ! under DOE Acquisition Letter 97-1.
-! 
-! 
+!
+!
 ! DISCLAIMER
-! 
-! This work was prepared as an account of work sponsored by an agency 
-! of the United States Government. Neither the United States Government 
-! nor the University of California nor any of their employees, makes 
-! any warranty, express or implied, or assumes any liability or 
-! responsibility for the accuracy, completeness, or usefulness of any 
-! information, apparatus, product, or process disclosed, or represents 
-! that its use would not infringe privately-owned rights.  Reference 
-! herein to any specific commercial products, process, or service by 
-! trade name, trademark, manufacturer or otherwise does not necessarily 
-! constitute or imply its endorsement, recommendation, or favoring by 
-! the United States Government or the University of California. The 
-! views and opinions of authors expressed herein do not necessarily 
-! state or reflect those of the United States Government or the 
-! University of California, and shall not be used for advertising or 
+!
+! This work was prepared as an account of work sponsored by an agency
+! of the United States Government. Neither the United States Government
+! nor the University of California nor any of their employees, makes
+! any warranty, express or implied, or assumes any liability or
+! responsibility for the accuracy, completeness, or usefulness of any
+! information, apparatus, product, or process disclosed, or represents
+! that its use would not infringe privately-owned rights.  Reference
+! herein to any specific commercial products, process, or service by
+! trade name, trademark, manufacturer or otherwise does not necessarily
+! constitute or imply its endorsement, recommendation, or favoring by
+! the United States Government or the University of California. The
+! views and opinions of authors expressed herein do not necessarily
+! state or reflect those of the United States Government or the
+! University of California, and shall not be used for advertising or
 ! product endorsement purposes.
 
 
-     
+
 !.....MILI data types
 
 !     m_string           Character string
@@ -66,7 +66,7 @@
 !     m_int              Integer (default size)
 !     m_int4             Integer, 32-bit
 !     m_int8             Integer, 64-bit
-   
+
 !.....MILI object data superclasses.
 
 !     m_unit             Generic object
@@ -175,7 +175,7 @@
      &    m_class_exists, m_lib_version,                                &
      &    m_state_size, m_qty_facets_in_surface,                        &
      &    m_max_svars, m_max_array_dims, m_max_name_len,                &
-     &    m_max_path_len, m_max_preamble_len, m_max_string_len,         & 
+     &    m_max_path_len, m_max_preamble_len, m_max_string_len,         &
      &    m_too_many_scalars, m_too_many_fields, m_list_obj_fmt,        &
      &    m_block_obj_fmt, m_per_object, m_per_facet, m_dont_care,      &
      &    m_time_of_state, m_mesh_name
@@ -220,7 +220,7 @@
      &    m_per_object = 0, m_per_facet = 1,                            &
 
      &    m_dont_care = 0, m_blank = ' ',                               &
-      
+
      &    m_too_many_scalars = 1, m_too_many_fields = 2 )
 
 
